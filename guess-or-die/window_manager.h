da807cc35b26b0e9083e7d707fef40652d70fb5b
kammann123 <lucaskammann@hotmail.com>
2018-05-16 14:40:13 -0300

    window_manager header and source files added

---
+++ guess-or-die/window_manager.h
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   window_manager.h
+ * Author: lucas
+ *
+ * Created on 16 de mayo de 2018, 14:37
+ */
+
+#ifndef WINDOW_MANAGER_H
+#define WINDOW_MANAGER_H
+
+#endif /* WINDOW_MANAGER_H */
